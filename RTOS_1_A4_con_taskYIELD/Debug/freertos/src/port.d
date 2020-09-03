b82523ab4ee7ff5503610d3bc8ea1c48375ad262
milton-arg <milton.eduardo.sosa@gmail.com>
2020-09-03 23:20:53 +0200

    Se agrega solucion del ejercicio A4 con taskYIELD

---
+++ RTOS_1_A4_con_taskYIELD/Debug/freertos/src/port.d
@@ -0,0 +1,25 @@
+freertos/src/port.o freertos/src/port.d: ../freertos/src/port.c \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/FreeRTOS.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/projdefs.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/example/inc/FreeRTOSConfig.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/portable.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/portmacro.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/mpu_wrappers.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/task.h \
+ /home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/list.h
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/FreeRTOS.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/projdefs.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/example/inc/FreeRTOSConfig.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/portable.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/portmacro.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/mpu_wrappers.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/task.h:
+
+/home/snorkman/LPCXpresso/borra/freertos_button_A1/freertos/inc/list.h:
